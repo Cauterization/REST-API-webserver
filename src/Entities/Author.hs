5b77e0ab2b18a3a73e08674669e70f230079696f
Cauterization <71918928+Cauterization@users.noreply.github.com>
2022-03-29 19:50:56 +0300

    get authors

--- src/Entities/Author.hs
+++ src/Entities/Author.hs
@@ -3,10 +3,13 @@ module Entities.Author where
 
 import Data.Aeson (FromJSON, ToJSON)
 import Data.Text (Text)
+import Data.List (intercalate)
+import Data.String (fromString)
 
 import Database.Postgres(Postgres)
 import Database.PostgreSQL.Simple qualified as Postgres
 import Database.PostgreSQL.Simple.FromRow qualified as Postgres
+
 import GHC.Generics ( Generic )
 
 import Server.Router
@@ -20,7 +23,7 @@ import HKD.Filter (Filter)
 import HKD.Front ( Front )
 import HKD.Update (Update, Immutable)
 
-import Entities.Internal ( EntityOrID )
+import Entities.Internal ( EntityOrID, fieldsQuery )
 import Entities.User 
 
 import Database.Database qualified as Database
@@ -28,10 +31,9 @@ import Database.Database qualified as Database
 import Data.Data
 
 data Author a = Author
-  { user        :: Field "user"        'Required a '[Immutable]        (EntityOrID User a)
-  , description :: Field "description" 'Required a '[]                                Text
+  { user        :: Field "user"        'Required a '[Immutable] (EntityOrID User a)
+  , description :: Field "description" 'Required a '[]          Text
   } deriving stock (Generic)
-    deriving (Database.Entity Postgres)
 
 deriving instance EmptyData (Author Update)
 deriving instance FromJSON  (Author Update)
@@ -40,13 +42,21 @@ deriving instance EmptyData (Author Filter)
 
 deriving instance FromJSON  (Author (Front Create))
 
-deriving instance ToJSON    (Author Display)
-deriving instance Data      (Author Display)
-instance Postgres.FromRow   (Author Display) where
+deriving instance ToJSON    (Author (Front Display))
+deriving instance Data      (Author (Front Display))
+instance Postgres.FromRow   (Author (Front Display)) where
     fromRow =  do
         user        <- Postgres.fromRow
         description <- Postgres.field
         pure Author{..}
+
+instance Database.DBEntity Postgres Author where
+    getEQuery = mconcat 
+        [ "SELECT "
+        , fieldsQuery @(User (Front Display))
+        , ", description" 
+        , " FROM authors_view"
+        ]
 
 instance Routed Author Postgres where
     router = do
