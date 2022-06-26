97df5e0f82dc451c13916aa29ee4dd2dea441014
Cauterization <71918928+Cauterization@users.noreply.github.com>
2022-06-26 06:48:39 +0300

    #6

--- sh-scripts/UserPost.sh
+++ sh-scripts/UserPost.sh
@@ -7,5 +7,5 @@ echo Enter login
 read login
 echo Enter password
 read password
-body={\"first_name\":\"$firstname\",\"last_name\":\"$lastname\",\"login\":\"$login\",\"password\":\"$password\",\"admin\":false}
+body={\"first_name\":\"$firstname\",\"last_name\":\"$lastname\",\"login\":\"$login\",\"password\":\"$password\"}
 echo $(curl -X POST -H "Content-Type: application/json" -d $body http://localhost:3000/users)
